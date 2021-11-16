fecc43582de005e457540bd7de35bbab68535afc
Soni Mohandas <sonireetha@gmail.com>
2021-11-15 22:55:39 -0500

    file edited

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bafca2c41f1e3d5d71a8971d9bad2e53'
 
 posts = [
-
     {
         'author': 'Soni Mohandas',
         'title': 'Blog Post 1',
@@ -26,6 +27,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',posts = posts, title = 'About')
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
 
 if __name__ == "__main__":    
     app.run(debug = True)
