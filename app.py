e18b94a2d561ec8325eaa01ca2ed0931068a7288
Soni Mohandas <sonireetha@gmail.com>
2021-11-16 18:26:41 -0500

    file added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ posts = [
         'date_posted': 'April 20, 2021'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Harigovind Soni',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 20, 2021'
@@ -28,14 +28,23 @@ def home():
 def about():
     return render_template('about.html',posts = posts, title = 'About')
 
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful!, Please enter valid username and password', 'danger')            
     return render_template('login.html', title = 'Login', form = form)
 
-@app.route("/register")
+@app.route("/register", methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form = form)
 
 if __name__ == "__main__":    
